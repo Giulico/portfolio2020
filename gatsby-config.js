0aef05693d28ab8753fae81d2154684c6f72139d
giuliocollesei <giuliocollesei@gmail.com>
2019-07-23 21:21:17 +0200

    removed static/img folter from gatsby-source-filesistem plugin config

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,14 +39,14 @@ module.exports = {
         ]
       }
     },
-    {
-      // keep as first gatsby-source-filesystem plugin for gatsby image support
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/static/img`,
-        name: 'uploads'
-      }
-    },
+    // {
+    //   // keep as first gatsby-source-filesystem plugin for gatsby image support
+    //   resolve: 'gatsby-source-filesystem',
+    //   options: {
+    //     path: `${__dirname}/static/img`,
+    //     name: 'uploads'
+    //   }
+    // },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
